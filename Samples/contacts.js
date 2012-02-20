70dc5ecfe0682f0e7fa56f1986b2b5767f0ea6bd
Ian Anderson <ipanderson@gmail.com>
2012-02-20 14:21:08 +0000

    Added JS as well

---
+++ Samples/contacts.js
@@ -0,0 +1,47 @@
+var contacts = {
+
+	sendPropertyForm : function(e) {
+		e.preventDefault();
+		$this = $(this);
+		$form = $this.closest('form');
+		if ($form.find('input[name="value"]').val() != '') {
+			$.post($form.attr('action'), $form.serialize(), function (result) {
+				contacts.refreshList($this, result);
+			});
+		}
+	},
+	
+	updateList : function(e) {
+		var contactID = getIDValue($(this).attr('id'));
+		var propertyIDs = $(this).sortable('serialize');
+		$.post('/contacts/contacts/update-properties/id/' + contactID, propertyIDs, function (result) {
+			console.log('sent');
+			contacts.refreshList($this, result);
+		});
+
+	},
+	
+	refreshList : function($this, result) {
+		$list = $this.closest('ul');
+		$list.replaceWith(result);
+		contacts.hideAllOptions();
+		contacts.sortable();
+	}, 
+	
+	
+	showOptions : function () {
+		$this = $(this);
+		$this.find('.hide').show();
+	},
+	
+	hideOptions : function () {
+		$this = $(this);
+		$this.find('.hide').hide();
+	}
+	
+}
+
+$(document).ready(contacts.init);
+
+
+
